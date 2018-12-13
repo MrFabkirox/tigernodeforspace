a87fb7f88ccfda029e2d9291f42bfffca88c6c85
Fabrice Esope <fabrice.esope@gmail.com>
2018-12-13 14:24:35 +0800

    add first front test

--- tigerclient/src/App.test.js
+++ tigerclient/src/App.test.js
@@ -1,3 +1,11 @@
+// react-testing-library renders your components to document.body,
+// this will ensure they' re removed after  each test.
+import 'react-testing-library/cleanup-after-each'
+
+// this adds jest-dom's custom assertions
+import 'jest-dom/extend-expect'
+import { render, wait, waitForElement } from 'react-testing-library'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -6,4 +14,13 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  const { getByText } = render(<App />);
+  expect(
+    getByText('Let\'s share some interesting articles')
+  ).toBeInTheDOM()
+
+  // ReactDOM.unmountComponentAtNode(div); replaced w cleanup-a-e
 });
